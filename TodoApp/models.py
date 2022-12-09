5b33869abddee5d13252ada30b52335afaded41d
ashis-solomon <ashissolomon24@gmail.com>
2022-12-09 23:46:55 +0530

    update

---
+++ TodoApp/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Integer, Column, String
+from database import Base
+
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(String)
+    complete = Column(Boolean, default=False)
