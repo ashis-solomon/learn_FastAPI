ee63a862f8f3adfed625b6a8a4a82c7c3ffd09a0
ashis-solomon <ashissolomon24@gmail.com>
2022-12-09 20:48:18 +0530

    update

--- project2/books2.py
+++ project2/books2.py
@@ -10,19 +10,111 @@ app = FastAPI()
 class Book(BaseModel):
     id: UUID
     title: str = Field(min_length=1)
-    author: str
+    author: str = Field(min_length=1, max_length=100)
     description: Optional[str] = Field(min_length=1, max_length=100)
-    rating: int
+    rating: int = Field(ge=0, le=100)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "0526f5e7-3246-4a8c-801f-d426e708ac1e",
+                "title": "title1",
+                "author": "author1",
+                "description": "desc1",
+                "rating": 1
+            }
+        }
+
 
 
 BOOKS = []
 
 
 @app.get('/')
-async def read_all_books():
+async def read_all_books(limit: Optional[int] = None):
+    BOOKS_copy = []
+    if limit:
+        i=0
+        for x in BOOKS:
+            i+=1
+            BOOKS_copy.append(x)
+            if i == limit:
+                break
+        print(BOOKS_copy)
+        return BOOKS_copy
+
     return BOOKS
+
+@app.get('/book/{book_id}')
+async def read_book(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    return {'detail': 'Book not Found'}
 
 @app.post('/')
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+@app.put('/{book_id}')
+async def update_book(book_id: UUID, book: Book):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS[counter-1] = book
+            return book
+    return {'detail': 'Book not Found'}
+
+@app.delete('/{book_id}')
+async def delete_book(book_id: UUID):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:          
+            return BOOKS.pop(counter-1)
+    return {'detail': 'Book not Found'}
+
+
+
+def create_books_no_api():
+    book1 = Book(
+        id = "0526f5e7-3246-4a8c-801f-d426e708ac1e",
+        title = "title1",
+        author= "author1",
+        description= "desc1",
+        rating = 1
+    )
+
+    book2 = Book(
+        id = "77c3e007-a2c4-4df1-a624-f6966d216d70",
+        title = "title2",
+        author= "author2",
+        description= "desc2",
+        rating = 2
+    )
+    
+    book3 = Book(
+        id = "b87a94d0-e3e8-40da-87a7-23d8110616b4",
+        title = "title3",
+        author= "author3",
+        description= "desc3",
+        rating = 3
+    )
+
+    book4 = Book(
+        id = "9b048731-96d9-499c-8c18-ff17d1d0358f",
+        title = "title4",
+        author= "author4",
+        description= "desc4",
+        rating = 4
+    )
+
+    BOOKS.append(book1)
+    BOOKS.append(book2)
+    BOOKS.append(book3)
+    BOOKS.append(book4)
+
+# creating dummy books
+create_books_no_api()
