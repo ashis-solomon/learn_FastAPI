ceac088f32e3d17a13f0840d3fa4aaf5ffafa549
ashis-solomon <ashissolomon24@gmail.com>
2022-12-10 19:46:51 +0530

    update

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException, status
-import models
+from pydantic import BaseModel, Field
+
 from typing import Optional
+
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
+
+import models
+from auth import get_current_user, get_user_exception
+
+
 
 app = FastAPI()
 
@@ -29,10 +35,22 @@ async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
 
+@app.get('/todos/user')
+async def read_all_by_user(user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+
+    return db.query(models.Todos).filter(models.Todos.owner_id == user.get("user_id")).all()
+
+
 @app.get('/todo/{todo_id}')
-async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+async def read_todo(todo_id: int, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    
     todo_model = db.query(models.Todos)\
         .filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == user["user_id"])\
         .first()
     if todo_model is not None: 
         return todo_model
